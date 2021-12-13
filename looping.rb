bbe7dec56e6b7c3119f1ac67d7a1fb4ae076e5c3
Hoa Le <Hoa_le9485@yahoo.com>
2021-12-13 13:29:18 -0500

    commit tree 14e1b598015b2ed39c9c191311b97fcfa66ca34a

--- looping.rb
+++ looping.rb
@@ -1,5 +1,10 @@
 def happy_new_year
-  # your code here
+  counter = 10
+  until counter == 0
+    puts counter
+    counter -= 1
+  end
+  puts "Happy New Year!"
 end
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
@@ -15,10 +20,19 @@ def fizzbuzz(num)
   end
 end
 
+# from 1..100  do num; num == 1 to 100 
+# do; is run num from 1 to 100
 def fizzbuzz_printer
-  # your code here
+  (1..100).each do |num|
+    # print the fnc fizzbuzz with the declaration of num == 1 to 100
+    puts fizzbuzz(num)
+  end
 end
 
 def reverse_string(str)
-  # your code here
+  reversed_str = ""
+  str.length.times do |i|
+    reversed_str = str[i] + reversed_str
+  end
+  reversed_str
 end
